f3f5c04f0697575a89f00217ac7b556a8323c6af
tavosalas270 <lordtavo27@gmail.com>
2024-08-30 18:08:12 -0500

    Revisando

--- models/mysql/movies.js
+++ models/mysql/movies.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const config = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '12345',
-    port: process.env.DB_PORT || 3306,
-    database: process.env.DB_NAME || 'moviesdb'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 }
 
 console.log(config)
